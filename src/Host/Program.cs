b3a76f0217a82c038e7f6868289636dbdf13618b
Alexy Sviridov <alexysviridov@mail.ru>
2024-04-15 10:29:50 +0300

    read excel

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OfficeOpenXml;
+using System.IO;
+using System;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.RefAndLookup;
 
 namespace Host
 {
@@ -10,7 +10,27 @@ namespace Host
     {
         static void Main(string[] args)
         {
+            FileInfo existingFile = new FileInfo(@"C:\Users\User\Desktop\ТЗиБ\K6. Info v1.20.xlsx");
+            using (ExcelPackage package = new ExcelPackage(existingFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[11];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count
 
+                //for (int row = 1; row <= rowCount; row++)
+                //{
+                //    for (int col = 1; col <= colCount; col++)
+                //    {
+                //        var cellValue = worksheet.Cells[row, col].Value;
+                //        if (cellValue != null)
+                //        {
+                //            Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + cellValue.ToString().Trim());
+                //        }
+                //    }
+                //}
+
+                Console.WriteLine(" Row:" + 13 + " column:" + 3 + " Value:" + worksheet.Cells[13, 3].Value.ToString().Trim());
+            }
         }
     }
 }
