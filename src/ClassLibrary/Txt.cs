4b245a533db533487d8e9e638b8ce854e62eb376
Alexy Sviridov <alexysviridov@mail.ru>
2024-04-15 10:53:07 +0300

    add create txt

---
+++ src/ClassLibrary/Txt.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+using System;
+
+namespace ClassLibrary
+{
+    public class Txt
+    {
+        public static void CreateTxt(string path)
+        {
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // Open the stream and read it back.
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.RefAndLookup;
+using ClassLibrary;
 
 namespace Host
 {
@@ -10,6 +11,8 @@ namespace Host
     {
         static void Main(string[] args)
         {
+            string pathTxt = @"C:\Users\User\Desktop\ТЗиБ\MyTest.txt";
+
             FileInfo existingFile = new FileInfo(@"C:\Users\User\Desktop\ТЗиБ\K6. Info v1.20.xlsx");
             using (ExcelPackage package = new ExcelPackage(existingFile))
             {
@@ -17,19 +20,9 @@ namespace Host
                 int colCount = worksheet.Dimension.End.Column;  //get Column Count
                 int rowCount = worksheet.Dimension.End.Row;     //get row count
 
-                //for (int row = 1; row <= rowCount; row++)
-                //{
-                //    for (int col = 1; col <= colCount; col++)
-                //    {
-                //        var cellValue = worksheet.Cells[row, col].Value;
-                //        if (cellValue != null)
-                //        {
-                //            Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + cellValue.ToString().Trim());
-                //        }
-                //    }
-                //}
-
                 Console.WriteLine(" Row:" + 13 + " column:" + 3 + " Value:" + worksheet.Cells[13, 3].Value.ToString().Trim());
+
+                Txt.CreateTxt(pathTxt);
             }
         }
     }
