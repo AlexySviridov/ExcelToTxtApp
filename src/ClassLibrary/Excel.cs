05bb873e6fd6578dcc56b00d220cb9a8580d2181
Alexy Sviridov <alexysviridov@mail.ru>
2024-06-27 14:03:13 +0300

    add armature type

--- src/ClassLibrary/Excel.cs
+++ src/ClassLibrary/Excel.cs
@@ -9,8 +9,16 @@ namespace ClassLibrary
 {
     public class Excel
     {
-        readonly string[] commandsArray = new[] { "Закр", "Откр", "Вкл", "Откл" };
-        readonly string[] bansArray = new[] { "ЗапО", "ЗапЗ" };
+        readonly static string[] bansArray = new[] { "ЗапО", "ЗапЗ" };
+        readonly static string[] commandsArray = new[] { "Закр", "Откр", "Вкл", "Откл" };        
+        enum TypeArmature
+        {
+            BansNotExists,
+            CommandsNotExist, 
+            BansAndCommandsExistInFirstField, 
+            CommandsExistInSecondField,
+            UnidentifiedType
+        }
         public static void Read(string pathToExcel, int numberWorksheets, int[] ignoredRowsArray, int firstArmatureRow, int ArmatureNameColumn, int firstAlgorithmColumn)
         {
             FileInfo existingFile = new FileInfo(pathToExcel);
@@ -29,10 +37,39 @@ namespace ClassLibrary
                         if (worksheet.Cells[i, j].Value == null) continue;
                         armature.values.Add(worksheet.Cells[i, j].Value.ToString().Trim());
                     }
-                    //foreach (string value in armature.values) Console.Write(value + " ");
-                    //Console.WriteLine();
+                    typeArmature(armature);
                 }
             }
+        }
+
+        private static TypeArmature typeArmature(Armature armature)
+        {
+            var bansInFirstField = false;
+            var commandsInFirstField = false;
+            var commandsInSecondField = false;
+
+            foreach (string value in armature.values)
+            {
+                if (value.Split('/').Length == 1) 
+                {
+                    var firstFieldValue = value.Split('/')[0];
+                    if (bansArray.Contains(firstFieldValue)) bansInFirstField = true;
+                    else if (commandsArray.Contains(firstFieldValue)) commandsInFirstField = true;
+                    else throw new Exception("Неопознанный запрет или команда: " + firstFieldValue);
+                }
+                else if (value.Split('/').Length > 1)
+                {
+                    var secondFieldValue = value.Split('/')[1];
+                    if (commandsArray.Contains(secondFieldValue)) commandsInSecondField = true;
+                    else if (secondFieldValue != "Руч") throw new Exception("Неопознанный запрет или команда: " + secondFieldValue);
+                }                
+            }
+
+            if (!bansInFirstField) return TypeArmature.BansNotExists;
+            else if (!commandsInFirstField && !commandsInSecondField) return TypeArmature.CommandsNotExist;
+            else if (bansInFirstField && commandsInFirstField) return TypeArmature.BansAndCommandsExistInFirstField;
+            else if (bansInFirstField && commandsInSecondField) return TypeArmature.CommandsExistInSecondField;
+            else return TypeArmature.UnidentifiedType;
         }
     }
 }
